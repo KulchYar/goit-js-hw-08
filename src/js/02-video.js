c34486c5018bef5b0ab6fa72ae29f3d41236f841
KulchYar <108682555+KulchYar@users.noreply.github.com>
2023-10-04 00:06:13 +0300

    1.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+function saveTime(time) {
+    localStorage.setItem('videoplayer-time, time')
+};
+   
+function loadTIme() {
+ return   localStorage.getItem('videoplayer-time')
+}
